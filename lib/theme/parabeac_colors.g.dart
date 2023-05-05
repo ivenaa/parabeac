6087b975c6d3f0788f7d61ba2e5c875eed1de35d
Parabeac-Cloud <admin@parabeac.com>
2023-05-05 05:58:51 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/parabeac_colors.g.dart
@@ -0,0 +1,38 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class ParabeacColors {
+
+static const Color primary = Color(0xff5256e8);
+
+static const Color white = Color(0xffffffff);
+
+static const Color lightGrey = Color(0xffdedee8);
+
+static const Color secondary = Color(0xfff26060);
+
+static const Color fontPrimary = Color(0xff19104e);
+
+static const Color fontGrey = Color(0xffa9a9aa);
+
+static const Color lightPurple = Color(0xfff5f5ff);
+
+static const Color iOS000000 = Color(0xff000000);
+
+static const Color iOSFFFFFF = Color(0xffffffff);
+
+static const Color iOS50555C = Color(0xff50555c);
+
+static const Color iOSAlfaCCCED376% = Color(0xc2ccced3);
+
+static const Color iris100 = Color(0xff5d5fef);
+
+static const Color lightGrey2 = Color(0xfff1f1f5);
+
+static const Color primary2 = Color(0xff5256e8);
+}
---
+++ lib/theme/parabeac_text_styles.g.dart
@@ -0,0 +1,30 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class ParabeacTextStyles {
+
+static const TextStyle 24Bold = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w100,
+  letterSpacing: 0,
+  fontFamily: 'Roboto',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.italic,
+)
+;
+
+static const TextStyle 18Heavy = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w100,
+  letterSpacing: 0,
+  fontFamily: 'Roboto',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.italic,
+)
+;
+}
